7607cddaf99c4da6b3315e2cb0935876b945987d
Grimlin238 <ty.lashley14@icloud.com>
2023-04-26 13:07:59 -0400

    Edited index.js file to reflect requirements of database. Added middle ware for processing data. Added comments so contributors know what to do.

--- app/APWProject/index.js
+++ app/APWProject/index.js
@@ -1,6 +1,11 @@
 let express= require('express');
 
 let app = express();
+app.use(express.urlencoded({
+	extended: true 
+}))
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
 	
@@ -9,7 +14,7 @@ res.send('<h1> Welcome to insert app name </h1> <ul> <li> <a href="/create"> Cre
 
 app.get('/create', (req, res) => {
 	
-	res.send('<h1> Create an account </h1><form method="post"><h2> First Name </h2><input name="FirstName"><h2> Last name </h2><input name="LastName"><h2> User name </h2><input name="UserName"><input type="submit" value="Create Account"></form>');
+	res.send('<h1> Create an account </h1><form method="post"><h2> User Name </h2><input name="userName"><h2> Password </h2><input name="passWord"><input type="submit" value="Create Account"></form>');
 });
 
 app.get('/login', (req, res) => {
@@ -17,6 +22,38 @@ app.get('/login', (req, res) => {
 	res.send('<h1> Log in <h1><form method="post"><h2> User name </h2><input name="USerName"><input type="submit" value="Log in"></form>');
 });
 
+app.post('/create', (req, res) => {
+	
+	const{userName, passWord} = req.body;
+	
+	if (userName === "" && passWord === "") {
+		
+		res.send("Error creating ccount. Please go back and ensure all fields are inserted")
+	}
+	
+	// Below this comment
+	// please add user name to database as well as password
+	//
+	
+	else {
+		
+		// database handling code goes here
+		// I will continue where you left off afterwords
+		
+	} 
+	
+	
+})
+
+app.post('/login', (req, res) =>
+
+{
+	
+	const(userName, passWord) = req.body;
+	
+	// Check if user name and passowrd is correct
+	
+})
 app.listen(8080, () => {
 	
 	console.log("I'm listening on port 8080");
